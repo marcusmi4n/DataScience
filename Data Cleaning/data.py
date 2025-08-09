7ec9c7fe915060b20a735d639df599a7e66c0abf
marcusmi4n <marcuslutas@gmail.com>
2025-08-09 11:11:13 +0300

    new

--- Data Cleaning/data.py
+++ Data Cleaning/data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('./dataset/mydataset.csv')
+df = pd.read_csv('/dataset/mydataset.csv')
 print(df.head())
 print(df.info())
 print(df.describe())
